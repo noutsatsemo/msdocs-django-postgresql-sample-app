329f11311416cd6b8cfc7b10562ca46ccfb6bf52
noutsatsemo <63719879+noutsatsemo@users.noreply.github.com>
2025-03-09 15:59:07 +0100

    add snippets from django tutorial

--- restaurant_review/models.py
+++ restaurant_review/models.py
@@ -1,6 +1,14 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+
 # Create your models here.
 
 class Restaurant(models.Model):
@@ -10,6 +18,22 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+    class Meta:
+        ordering = ['created']
+
+
+
 
 
 class Review(models.Model):
