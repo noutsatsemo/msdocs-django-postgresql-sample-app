c49f8abc73c3f6f6521d8dff67abfaa9c5cfee76
noutsatsemo <63719879+noutsatsemo@users.noreply.github.com>
2025-03-09 16:02:08 +0100

    add snippets from django tutorial

--- restaurant_review/views.py
+++ restaurant_review/views.py
@@ -1,12 +1,15 @@
 from django.db.models import Avg, Count
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.cache import cache_page
+from rest_framework.parsers import JSONParser
 
-from restaurant_review.models import Restaurant, Review
+from restaurant_review.serializers import SnippetSerializer
+
+from restaurant_review.models import Restaurant, Review, Snippet
 
 # Create your views here.
 
@@ -48,6 +51,52 @@ def add_restaurant(request):
         Restaurant.save(restaurant)
 
         return HttpResponseRedirect(reverse('details', args=(restaurant.id,)))
+
+
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
 
 
 @csrf_exempt
