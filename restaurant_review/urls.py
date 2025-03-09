8da31a6346d2854ae044c959efd40724a93bb3ef
noutsatsemo <63719879+noutsatsemo@users.noreply.github.com>
2025-03-09 16:01:27 +0100

    add snippets from django tutorial

--- restaurant_review/urls.py
+++ restaurant_review/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.details, name='details'),
     path('create', views.create_restaurant, name='create_restaurant'),
     path('add', views.add_restaurant, name='add_restaurant'),
     path('review/<int:id>', views.add_review, name='add_review'),
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+
 ]
