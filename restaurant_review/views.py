a0842ea83379237e1150afecc1e8fc45c66b2cdd
noutsatsemo <63719879+noutsatsemo@users.noreply.github.com>
2025-03-16 18:09:24 +0100

    Update views.py

--- restaurant_review/views.py
+++ restaurant_review/views.py
@@ -7,117 +7,56 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.cache import cache_page
 from rest_framework.parsers import JSONParser
 
-from restaurant_review.serializers import SnippetSerializer
-
-from restaurant_review.models import Restaurant, Review, Snippet
-
+from restaurant_review.models import Restaurant, Review
 # Create your views here.
 
-def index(request):
-    print('Request for index page received')
-    restaurants = Restaurant.objects.annotate(avg_rating=Avg('review__rating')).annotate(review_count=Count('review'))
-    lastViewedRestaurant = request.session.get("lastViewedRestaurant", False)
-    return render(request, 'restaurant_review/index.html', {'LastViewedRestaurant': lastViewedRestaurant, 'restaurants': restaurants})
 
-@cache_page(60)
-def details(request, id):
-    print('Request for restaurant details page received')
-    restaurant = get_object_or_404(Restaurant, pk=id)
-    request.session["lastViewedRestaurant"] = restaurant.name
-    return render(request, 'restaurant_review/details.html', {'restaurant': restaurant})
-
-
-def create_restaurant(request):
-    print('Request for add restaurant page received')
-    return render(request, 'restaurant_review/create_restaurant.html')
-
-
-@csrf_exempt
-def add_restaurant(request):
-    try:
-        name = request.POST['restaurant_name']
-        street_address = request.POST['street_address']
-        description = request.POST['description']
-    except (KeyError):
-        # Redisplay the form
-        return render(request, 'restaurant_review/add_restaurant.html', {
-            'error_message': "You must include a restaurant name, address, and description",
-        })
-    else:
-        restaurant = Restaurant()
-        restaurant.name = name
-        restaurant.street_address = street_address
-        restaurant.description = description
-        Restaurant.save(restaurant)
-
-        return HttpResponseRedirect(reverse('details', args=(restaurant.id,)))
+from restaurant_review.models import  Restaurant, Review, SillaUser, SillaProject
+from restaurant_review.serializers import SillaUserSerializer, SillaProjectSerializer, UserSerializer
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from restaurant_review.permissions import IsOwnerOrReadOnly
 
 
 
-@csrf_exempt
-def snippet_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-@csrf_exempt
-def snippet_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return HttpResponse(status=204)
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-@csrf_exempt
-def add_review(request, id):
-    restaurant = get_object_or_404(Restaurant, pk=id)
-    try:
-        user_name = request.POST['user_name']
-        rating = request.POST['rating']
-        review_text = request.POST['review_text']
-    except (KeyError):
-        # Redisplay the form.
-        return render(request, 'restaurant_review/add_review.html', {
-            'error_message': "Error adding review",
-        })
-    else:
-        review = Review()
-        review.restaurant = restaurant
-        review.review_date = timezone.now()
-        review.user_name = user_name
-        review.rating = rating
-        review.review_text = review_text
-        Review.save(review)
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    return HttpResponseRedirect(reverse('details', args=(id,)))
+
+class SillaUserList(generics.ListCreateAPIView):
+    queryset = SillaUser.objects.all()
+    serializer_class = SillaUserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+
+class SillaUserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SillaUser.objects.all()
+    serializer_class = SillaUserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]    
+
+
+
+class SillaProjectList(generics.ListCreateAPIView):
+    queryset = SillaProject.objects.all()
+    serializer_class = SillaProjectSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SillaProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SillaProject.objects.all()
+    serializer_class = SillaProjectSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
