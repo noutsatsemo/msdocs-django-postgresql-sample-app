a7facbe09a3351bf70362fd8dbfdc649e6157119
noutsatsemo <63719879+noutsatsemo@users.noreply.github.com>
2025-03-16 17:57:59 +0100

    Update serializers.py  add login

--- restaurant_review/serializers.py
+++ restaurant_review/serializers.py
@@ -1,14 +1,34 @@
-
-
 from rest_framework import serializers
-from restaurant_review.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+from restaurant_review.models import SillaProject, SillaUser, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
+    sillausers = serializers.PrimaryKeyRelatedField(many=True, queryset=SillaUser.objects.all())
+    sillaprojects = serializers.PrimaryKeyRelatedField(many=True, queryset=SillaProject.objects.all())
+
     class Meta:
-        model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        model = User
+        fields = ['id', 'username', 'sillausers','sillaprojects']
 
+
+
+class SillaProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = SillaProject
+        fields = ['id', 'name', 'owner',  'description', 'location', 'total_material_cost_price', 'total_labor_cost_price', 'total_other_cost_price']
+
+
+class SillaUserSerializer(serializers.ModelSerializer):    
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = SillaUser
+        fields = ['id', 'name', 'email', 'phone', 'birthday', 'owner']
 
 
 
