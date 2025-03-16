4d86faf436100e9516ae1c2de4c3ad10b6bca5cb
noutsatsemo <63719879+noutsatsemo@users.noreply.github.com>
2025-03-16 18:18:50 +0100

    Update settings.py

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -35,6 +35,7 @@ if 'CODESPACE_NAME' in os.environ:
 
 INSTALLED_APPS = [
     'restaurant_review.apps.RestaurantReviewConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
